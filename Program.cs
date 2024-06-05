7d3acb43e76e6fd8e1f3d48b5504830f5c70673a
Garrett <Garrett@YUP>
2024-06-05 11:02:25 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Assignment5._2._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word");
+            string word = Console.ReadLine();
+            
+            bool result = IsPalindrome(word, 0, word.Length-1);
+            if (result)
+            {
+                Console.WriteLine($"{word} is a Palindrome");
+            }
+            else
+            {
+                Console.WriteLine($"{word} is not a Palindrome");
+            }
+        }
+
+        static bool IsPalindrome(string word, int start, int end)
+        {
+            if (start >= end  )
+            {
+                return true;
+            }
+
+            if (word[start] != word[end])
+            {
+                return false;
+            }
+            return IsPalindrome(word, start + 1, end - 1);
+        }
+
+       
+    }
+}
